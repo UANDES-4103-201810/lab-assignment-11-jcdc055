0796a8cc869408ac2afca00f356b9a120a0ee11f
juanignacio1509 <jdiaz@miuandes.cl>
2018-06-05 14:58:31 -0400

    lab11

--- db/migrate/20180605180728_create_messages.rb
+++ db/migrate/20180605180728_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
       t.date :date
-      t.string :content
+      t.string :content 
 
       t.references :user, foreign_key: true
 
